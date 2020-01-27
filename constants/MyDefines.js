2a60b716f343f9be5d8084e39f214c6eb267e3e9
Ribity <Ribity@yahoo.com>
2020-01-26 22:14:35 -0500

    Jan 26th 3rd update

--- constants/MyDefines.js
+++ constants/MyDefines.js
@@ -6,7 +6,7 @@ export default {
     sentry_logging: true,
     console_log_breadcrumbs: true,
     playEndOfStoryRibbit: true,
-    ribbit_wav_num: 3,
+    ribbit_wav_num: 4,
     keep_awake: true,
 
     myStatusBarHeight: Constants.statusBarHeight,
